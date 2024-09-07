2b7e05779f29859cebaf1bf9d43692a148996574
coolboydidi <deinbojack5606@gmail.com>
2024-09-07 16:47:40 -0500

    Personal Website - Version 1
    Very beginning stage of my personal website. This is all I have saved locally.

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".switching-text", {
+  strings: ["Service.", "Environmentalism.", "Fashion Design."],
+  typeSpeed: 100, 
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+
